60730a2fa4f01bf2f21dba2c89ef0ab156315b3e
admin <admin@edelweis1254.krvostok.ru>
2025-03-06 17:10:28 +0300

    no changes

--- db/db.go
+++ db/db.go
@@ -71,6 +71,62 @@
 //	log.Println("Database connected")
 //}
 
+//package db
+//
+//import (
+//	"database/sql"
+//	"fmt"
+//	"log"
+//	"os"
+//	"time"
+//
+//	"github.com/joho/godotenv"
+//	_ "github.com/lib/pq" // Драйвер для PostgreSQL
+//)
+//
+//var DB *sql.DB // Глобальная переменная для соединения с БД
+//
+//func init() {
+//	err := godotenv.Load()
+//	if err != nil {
+//		log.Println("Error loading .env file")
+//	}
+//
+//	// Получаем параметры БД
+//	dbHost := os.Getenv("DB_HOST")
+//	dbPort := os.Getenv("DB_PORT")
+//	dbUser := os.Getenv("DB_USER")
+//	dbPassword := os.Getenv("DB_PASSWORD") // Может быть пустым
+//	dbName := os.Getenv("DB_NAME")
+//
+//	var connStr string
+//	if dbPassword == "" {
+//		connStr = fmt.Sprintf(
+//			"host=%s port=%s user=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbName,
+//		)
+//	} else {
+//		connStr = fmt.Sprintf(
+//			"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+//			dbHost, dbPort, dbUser, dbPassword, dbName,
+//		)
+//	}
+//
+//	// Добавляем задержку перед подключением к базе данных
+//	time.Sleep(5 * time.Second)
+//
+//	DB, err = sql.Open("postgres", connStr)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//
+//	errPing := DB.Ping()
+//	if errPing != nil {
+//		log.Fatal(errPing)
+//	}
+//
+//	log.Println("Database connected")
+//}
+
 package db
 
 import (
@@ -78,59 +134,65 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
-	_ "github.com/lib/pq" // Драйвер для PostgreSQL
+	_ "github.com/lib/pq"
 )
 
-var DB *sql.DB // Глобальная переменная для соединения с БД
+var DB *sql.DB
 
 func init() {
+	// Загружаем переменные окружения
 	err := godotenv.Load()
 	if err != nil {
-		log.Println("Error loading .env file")
+		log.Fatalf("Ошибка загрузки .env файла: %v", err)
 	}
 
+	// Получаем параметры БД
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD") // Может быть пустым
+	dbPassword := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
 
-	var connStr string
-	if dbPassword == "" {
-		connStr = fmt.Sprintf(
-			"host=%s port=%s user=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbName,
-		)
-	} else {
-		connStr = fmt.Sprintf(
-			"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-			dbHost, dbPort, dbUser, dbPassword, dbName,
-		)
-	}
+	// Формируем строку подключения
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName,
+	)
 
+	// Подключаемся к БД с повторными попытками
 	DB, err = sql.Open("postgres", connStr)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Ошибка подключения к БД: %v", err)
 	}
 
-	errPing := DB.Ping()
-	if errPing != nil {
-		log.Fatal(errPing)
+	// Проверяем подключение с задержкой
+	for i := 1; i <= 5; i++ {
+		err = DB.Ping()
+		if err == nil {
+			break
+		}
+		log.Printf("Попытка %d: ошибка проверки подключения: %v", i, err)
+		time.Sleep(2 * time.Second)
+	}
+	if err != nil {
+		log.Fatalf("Не удалось подключиться к БД: %v", err)
 	}
 
-	// Создание таблицы tasks, если она не существует
-	//_, err = DB.Exec(`
-	//    CREATE TABLE IF NOT EXISTS tasks (
-	//        id SERIAL PRIMARY KEY,
-	//        title VARCHAR(255) NOT NULL,
-	//        done BOOLEAN DEFAULT FALSE,
-	//        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-	//    )
-	//`)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	log.Println("Database connected")
+	// Создаем таблицу с логированием ошибок
+	log.Println("Создаем таблицу tasks...")
+	_, err = DB.Exec(`
+        CREATE TABLE IF NOT EXISTS public.tasks (
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            done BOOLEAN DEFAULT FALSE,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    `)
+	if err != nil {
+		log.Fatalf("Ошибка создания таблицы: %v", err)
+	}
+	log.Println("Таблица tasks создана успешно")
 }
